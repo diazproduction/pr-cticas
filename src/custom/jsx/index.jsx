580bee7c27b8b0b7594f3a4f10ede650b5119af5
Jorge Diaz <diazproduction@hotmail.es>
2015-12-03 00:08:39 +0000

    Add jsx folder and files for start messing around with React.

---
+++ src/custom/jsx/index.jsx
@@ -0,0 +1,7 @@
+'use strict'
+
+//var ReactDOM = require('react-dom');
+var React = require('react');
+var Hello = require('./hello');
+
+React.render(<Hello />, document.getElementById('content'));
